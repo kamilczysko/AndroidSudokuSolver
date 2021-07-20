4940ec4fbafb096d6de827efa9b6c70b9f61bf8b
Kamil <kamiwal92@gmail.com>
2021-07-20 22:50:24 +0200

    refactor pt1

--- app/src/main/java/com/kamil/opcvtest/ImageService.java
+++ app/src/main/java/com/kamil/opcvtest/ImageService.java
@@ -3,7 +3,7 @@ package com.kamil.opcvtest;
 import org.opencv.core.Mat;
 import org.opencv.core.Size;
 
-public class SudokuProcessor {
+public class ImageService {
 
     public void divideImage(Mat mat) {
         Size size = mat.size();
--- app/src/main/java/com/kamil/opcvtest/MainActivity.java
+++ app/src/main/java/com/kamil/opcvtest/MainActivity.java
@@ -32,6 +32,7 @@ import org.opencv.imgproc.Imgproc;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 public class MainActivity extends Activity implements CameraBridgeViewBase.CvCameraViewListener2, View.OnTouchListener {
@@ -47,8 +48,12 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     private TextView threshValue;
     private int threshVal = 3000;
     List<Rect> foundRects = new ArrayList();
-    Mat inputImage;
-    Rect selectedRect = null;
+    Mat originalRawInputImage;
+    Rect selectedSudokuPlane = null;
+    boolean isSudokuPlaneSelected = false;
+    Mat mat;
+    Mat previousFrame;
+    Mat previousFrameFromProcessedImage;
 
     Mat sudoku;
 
@@ -149,84 +154,106 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
 
     }
 
-    boolean selected = false;
-    Mat mat;
-    Mat lastScreen;
-    Mat orig;
+    Mat processedImage;
+    List<Rect> allCells;
+    Mat previousRawFrame;
+
     @SuppressLint("NewApi")
     @Override
     public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
         mat = new Mat();
-        Mat at;
+        processedImage = new Mat();
 
-        inputImage = inputFrame.rgba(); //todo input image ma byc niezalezne
-        Mat ccc = inputImage.clone(); // zamiast ccc uzyc input image
-        at = Mat.ones(inputImage.rows(), inputImage.cols(), inputImage.type());
-        Imgproc.cvtColor(inputImage, mat, Imgproc.COLOR_RGB2GRAY);
-        Imgproc.threshold(mat, mat, 120, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C);
-        if (selected) {
-            Mat selectedMap = lastScreen.submat(selectedRect).clone();
-            Mat or = orig.submat(selectedRect).clone();
+        originalRawInputImage = inputFrame.rgba();
+        Mat tmpRawFrame = originalRawInputImage.clone();
+        processedImage = originalRawInputImage.clone();
 
-            Imgproc.resize(selectedMap, selectedMap, inputImage.size(), 0, 0);
-            Imgproc.resize(or, or, inputImage.size(), 0, 0);
+        Imgproc.cvtColor(processedImage, processedImage, Imgproc.COLOR_RGB2GRAY);
+        Imgproc.threshold(processedImage, processedImage, 120, 255, Imgproc.ADAPTIVE_THRESH_MEAN_C);
+
+        if (isSudokuPlaneSelected) {
+            Mat processedSubmat = previousFrame.submat(selectedSudokuPlane).clone();
+            Mat originalSubmat = previousRawFrame.submat(selectedSudokuPlane).clone();
+
+            Imgproc.resize(processedSubmat, processedSubmat, processedImage.size(), 0, 0);
+            Imgproc.resize(originalSubmat, originalSubmat, processedImage.size(), 0, 0);
+
             setProcessButtonVisible();
-            sudoku = drawLines(selectedMap.clone(), or);
-            return sudoku;
+            return findSingleCells(processedSubmat, originalSubmat);
         }
 
-        List<Rect> foundRects = findContours(mat, inputImage); // input image jest kolorowany, to zmienic
-        foundRects.forEach(rect -> {
-                    Mat submat = at.submat(rect).clone();
-                    submat.setTo(new Scalar(255, 0, 0));
-                    submat.copyTo(inputImage.submat(rect), submat.clone());
-                }
-        );
+        findSudokuPlanesContours(processedImage, tmpRawFrame)
+                .forEach(sudokuPlaneBoudary ->
+                        getColorMaskForSingleSudokuPlane(sudokuPlaneBoudary, tmpRawFrame));
 
-        lastScreen = inputFrame.gray().clone();
-        orig = ccc.clone();
+        previousFrame = inputFrame.gray().clone();
+        previousFrameFromProcessedImage = processedImage.clone();
+        previousRawFrame  = originalRawInputImage.clone();
+
         setProcessButtonInvisible();
-        return inputImage;
+
+        return tmpRawFrame;
     }
 
-    private Mat drawLines(Mat sudoku, Mat original){
+    private void getColorMaskForSingleSudokuPlane(Rect sudokuPlaneBoudary, Mat frame) {
+        Mat dummyMat = Mat.ones(frame.rows(), frame.cols(), frame.type());
+        Mat submat = dummyMat.submat(sudokuPlaneBoudary).clone();
+        submat.setTo(new Scalar(255, 0, 0));
+        submat.copyTo(frame.submat(sudokuPlaneBoudary), submat.clone());
+    }
+
+
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    private Mat findSingleCells(Mat sudoku, Mat original) {
         Mat res = new Mat();
-        Imgproc.blur(sudoku, res, new Size(5,5), new Point(), 0);
+        allCells = new ArrayList<>();
+        Imgproc.blur(sudoku, res, new Size(5, 5), new Point(), 0);
         Imgproc.adaptiveThreshold(res, res, 255, Imgproc.ADAPTIVE_THRESH_GAUSSIAN_C, Imgproc.THRESH_BINARY, 5, 2);
         Core.bitwise_not(res, res);
         Imgproc.dilate(res, res, Imgproc.getStructuringElement(Imgproc.MORPH_ELLIPSE, new Size(5, 5)), new Point(), 11);
-        if(triger)
+        if (triger)
             return res;
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
-        Imgproc.findContours(res, contours, new Mat(),  Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
+        Imgproc.findContours(res, contours, new Mat(), Imgproc.RETR_TREE, Imgproc.CHAIN_APPROX_SIMPLE);
         Mat m = new Mat(sudoku.rows(), sudoku.cols(), CvType.CV_16F);
         original.copyTo(m);
         int c = 0;
-        for(MatOfPoint contour : contours){
+        for (MatOfPoint contour : contours) {
             if (Imgproc.contourArea(contour) > 2600 && Imgproc.contourArea(contour) < 40000) {
                 Rect rect = Imgproc.boundingRect(contour);
-                if (Math.abs(rect.height-rect.width) < 100) {
-                    Imgproc.rectangle(m, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255), 15);
-//                    Imgproc.circle(m, new Point(rect.x + rect.width / 2, rect.y + rect.height / 2), 3, new Scalar(0, 255, 255), 15);
-                    Imgproc.putText(m, (c)+"", new Point(rect.x + rect.width / 2, rect.y + rect.height / 2), 1, 1, new Scalar(0,255,0), 5);
-                    c++;
+                if (Math.abs(rect.height - rect.width) < 100) {
+                    allCells.add(rect);
                 }
             }
+        }
+        List<Rect> rects = allCells.stream()
+//                .sorted(Comparator.comparingInt(Rect::getY).thenComparing(Rect::getX))
+                .collect(Collectors.toList());
+        for (Rect rect : rects) {
+            Imgproc.rectangle(m, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255), 15);
+//                    Imgproc.circle(m, new Point(rect.x + rect.width / 2, rect.y + rect.height / 2), 3, new Scalar(0, 255, 255), 15);
+            Imgproc.putText(m, (c) + "", new Point(rect.x + rect.width / 2, rect.y + rect.height / 2), 1, 2, new Scalar(255, 0, 0), 15);
+            c++;
         }
         return m;
     }
 
-    private List<Rect> findContours(Mat input, Mat img) {
+    private List<Rect> findSudokuPlanesContours(Mat input, Mat img) {
         List<MatOfPoint> contours = new ArrayList<MatOfPoint>();
         Imgproc.findContours(input, contours, new Mat(), Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
         foundRects = new ArrayList();
         for (MatOfPoint a : contours) {
             if (Imgproc.contourArea(a) > 200) {
                 Rect rect = Imgproc.boundingRect(a);
-                if (rect.height > 200 && rect.width > 200 && rect.height < 500 && rect.width < 500) {
+                int width = rect.width;
+                int height = rect.height;
+                if (height > 200 && width > 200 && height < 500 && width < 500) {
                     foundRects.add(rect);
-                    Imgproc.rectangle(img, new Point(rect.x, rect.y), new Point(rect.x + rect.width, rect.y + rect.height), new Scalar(0, 0, 255), 15);
-                    Imgproc.circle(img, new Point(rect.x + rect.width / 2, rect.y + rect.height / 2), 3, new Scalar(0, 255, 255), 15);
+                    int x = rect.x;
+                    int y = rect.y;
+                    Imgproc.rectangle(img, new Point(x, y), new Point(x + width, y + height), new Scalar(0, 0, 255), 3);
+                    Imgproc.circle(img, new Point(x + width / 2, y + height / 2), 3, new Scalar(0, 255, 255), 3);
                 }
             }
         }
@@ -245,8 +272,8 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        int cols = mat.cols();
-        int rows = mat.rows();
+        int cols = originalRawInputImage.cols();
+        int rows = originalRawInputImage.rows();
 
         int xOffset = (camera.getWidth() - cols) / 2;
         int yOffset = (camera.getHeight() - rows) / 2;
@@ -254,20 +281,22 @@ public class MainActivity extends Activity implements CameraBridgeViewBase.CvCam
         int x = (int) event.getX() - xOffset;
         int y = (int) event.getY() - yOffset;
 
-        selectedRect = getSelectedRect(x, y);
-        selected = selectedRect != null;
+        selectedSudokuPlane = getSelectedSudoku(x, y);
+
+        isSudokuPlaneSelected = selectedSudokuPlane != null;
 
         return false;
     }
 
     @RequiresApi(api = Build.VERSION_CODES.N)
-    private Rect getSelectedRect(int x, int y) {
+    private Rect getSelectedSudoku(int x, int y) {
         return foundRects.stream()
-                .filter(rect -> isRectSelected(rect, x, y))
-                .findFirst().orElse(null);
+                .filter(rect -> isTouched(rect, x, y))
+                .findFirst()
+                .orElse(null);
     }
 
-    private boolean isRectSelected(Rect rect, int x, int y) {
+    private boolean isTouched(Rect rect, int x, int y) {
         return (x > rect.x && x < rect.x + rect.width) && (y > rect.y && y < rect.y + rect.height);
     }
 }
